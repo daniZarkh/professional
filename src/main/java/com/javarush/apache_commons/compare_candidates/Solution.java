e0ea6af279c64825a9c5b17c26f65d159f4bb9de
noslav7 <noslav7@gmail.com>
2022-08-31 09:00:50 +0500

    apache commons 1

---
+++ src/main/java/com/javarush/apache_commons/compare_candidates/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.apache_commons.compare_candidates;
+
+/*
+Сравни кандидатов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Candidate candidate1 = new Candidate("John", 38, "male", 185, 81, 15);
+        Candidate candidate2 = new Candidate("Omar", 44, "male", 175, 75, 15);
+
+        System.out.println(candidate1.equals(candidate2));
+    }
+}
