99d80b48bc9db8f73a0197eec9bd7399a2b0ca22
noslav7 <noslav7@gmail.com>
2022-08-31 10:29:29 +0500

    apache commons 1

---
+++ src/main/java/com/javarush/apache_commons/compare_cats/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.apache_commons.compare_cats;
+
+/*
+Сравниваем котов
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Bonya", 5, 4, "Devon Rex");
+        Cat cat2 = new Cat("Ginger", 5, 6, "Devon Rex");
+
+        System.out.println(cat1.equals(cat2));
+    }
+}
