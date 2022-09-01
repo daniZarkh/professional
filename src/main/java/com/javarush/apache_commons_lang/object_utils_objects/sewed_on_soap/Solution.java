d956bf058ed8d7342599bb49a12faab87ed71aa8
noslav7 <noslav7@gmail.com>
2022-09-01 11:32:08 +0500

    apache commons object_utils objects

---
+++ src/main/java/com/javarush/apache_commons_lang/object_utils_objects/sewed_on_soap/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.apache_commons_lang.object_utils_objects.sewed_on_soap;
+
+import org.apache.commons.lang3.ObjectUtils;
+
+import java.util.Objects;
+
+/*
+Шило на мыло
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String[] array1 = {null, "hello", "ok", null, "bye"};
+        String[] array2 = {"thanks", "hello", "ok", "bye"};
+
+        printFirstNonNull(array1);
+        System.out.println("----------");
+        printDefaultValueIfNullObject(array1, "oops");
+    }
+
+    public static void printFirstNonNull(final String... values) {
+        if (Objects.isNull(values)) {
+            System.out.println((Object) null);
+        }
+        for (final String value : values) {
+            if (Objects.nonNull(value)) {
+                System.out.println(value);
+                break;
+            }
+        }
+    }
+
+    public static void printDefaultValueIfNullObject(final String[] values, final String defaultValue) {
+        for (String o : values) {
+            System.out.println(Objects.requireNonNullElse(o, defaultValue));
+        }
+    }
+}
